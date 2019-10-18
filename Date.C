beea664716a8b82fff64d48a7fc293c543ae8fbc
alexowusu <owusu3y@gmail.com>
2019-10-18 19:02:57 +0100

    initial commit

---
+++ Date.C
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+/* 
+struct Date{
+int day,month,year;
+}
+
+int main(){
+ Date b{}
+ b.day=16;
+ b.month=5;
+ b.year=1991;
+  string mydate =to_string(b.year)+"/"+to_string(b.month)+"/"+to_string(b.day);
+ Date c{}
+  c.day=17;
+  c.month=9;
+  c.year = 1995;
+   string otherdate = 
+ if(b.year>c.year){cout<< Date b;}
+   else if(c.year > b.year){cout<<Date c;}
+else{
+if(b.month>c.month){cout<<Date b;}
+  else if(c.month>b.month){cout<<Date c;}
+else{ 
+      if(b.day>c.day){cout<< Date b;}
+     else if(c.day>b.day){cout<<Date c;}
+else{"dates are equal";}}
+}
+}*/
+
+//*********************conditional flow***************************
+int main(){
+
+
+int n.day,n.month,n.year;
+
+cout<< "Please Enter Your Date: ";
+cin >> n.day >> n.month >>n.year;
+
+Date n_date;
+n_date.day = n.day;
+n_date.month = n.month;
+n_date.year = n.year;
+
+int current = 20191018;
+string str_date = to_string(n_date.year)+to_string(n_date.month)+to_string(n_datte.day);
+string results = (stoi(str_date)) ? "your date is after current year":"your date is before current year"
+cout<< result <<edl;
+
+//*********************leap year*********************
+string leap_year= ((n_date.year % 4) == 0 )? "leap year": "not leap year";
+cout<< leap_year;
+
+//******************Proper dating*******************
+ 
+}
